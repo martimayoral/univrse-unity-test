f1e01e98d5c4021851cdb08b3d3bff3c5b2bcb44
martimayoral <76410086+martimayoral@users.noreply.github.com>
2023-11-10 14:05:07 +0100

    player border collision

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,7 +6,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed = 5;
-    Vector2 direction;
+    [SerializeField] KeyCode sprintKey = KeyCode.LeftShift;
+    float radius;
+
+    private void Awake()
+    {
+        radius = GetComponent<CircleCollider2D>().radius * transform.localScale.x;
+    }
 
     // Update is called once per frame
     void Update()
@@ -17,8 +23,16 @@ public class PlayerMovement : MonoBehaviour
 
         float inputSpeed = Mathf.Max(Mathf.Abs(x), Mathf.Abs(y));
 
+        if (Input.GetKey(sprintKey))
+            inputSpeed *= 2;
+
         // Move translation
         transform.Translate(Time.deltaTime * speed * inputSpeed, 0, 0);
+
+        // collision with bounds
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, SceneBounds.bounds.min.x + radius, SceneBounds.bounds.max.x - radius),
+            Mathf.Clamp(transform.position.y, SceneBounds.bounds.min.y + radius, SceneBounds.bounds.max.y - radius));
+
 
         if (inputSpeed > 0.1f)
             transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(y, x) * Mathf.Rad2Deg);
